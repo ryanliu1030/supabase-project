6c823e2f6702d342ccb781a380729a657175916e
Xinrui Liu <xinrui.liu.1030@gmail.com>
2023-02-01 23:25:03 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import MessageBoard from './MessageBoard'
+import AllPosts from './AllPosts'
+import PostView from './PostView'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import Welcome from './Welcome'
+import NavBar from './NavBar'
+import { SupabaseUserInfo, useSession } from './use-session'
+import { createContext } from 'react'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [{
+      path: "",
+      element: <MessageBoard />,
+      children: [
+        {
+          path: ":pageNumber",
+          element: <AllPosts />,
+        },
+        {
+          path: "post/:postId",
+          element: <PostView />,
+        },
+      ],
+    },
+    {
+      path: "welcome",
+      element: <Welcome />,
+    },
+  ],
+
+
+
+  },
+])
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <RouterProvider router={router} />;
+}
+
+export default App
+
+export const UserContext = createContext<SupabaseUSerInfo>({
+  session: null,
+  profile: null,
+
+})
+
+function Layout() {
+  const supabaseUserInfo = useSession();
+  return (
+  <>
+    <UserContext.Provider value={supabaseUserInfo}>
+      <NavBar />
+      <Outlet />
+    </UserContext.Provider>
+  </>
+  );
+}
---
+++ src/NavBar.tsx
@@ -0,0 +1,31 @@
+import { useContext } from "react" 
+import {Link} from 'react-router-dom'
+import { UserContext } from "./App"
+export default function NavBar(){
+    const {session} = useContext(UserContext)
+    return (
+        <>
+            <nav className='nav-bar'>
+                <Link className='nav-logo-link' to="/">
+                    <img
+                        id="logo"
+                        className="nav-logo"
+                        src="https://supaship.io/supaship_logo_with_text.svg"
+                        alt="logo"
+                    />
+                </Link>
+                <ul className="nav-right-list">
+                    <li className="nav-message-board-list-item">
+                        <Link to="/1" className='nav-message-board-link'>
+                            message board
+                        </Link>
+                    </li>
+                    <li className="nav-auth-item">
+                        {session?.user ? 'user is logged in' : 'user is logged out'}
+                    </li>
+                   
+                </ul>
+            </nav>
+        </>
+    )
+}
